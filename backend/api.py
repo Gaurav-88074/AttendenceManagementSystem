e28468d2bfd60e1101dba4a0d6c5a3632f1fe659
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-20 04:48:52 +0530

    Added routes

---
+++ backend/api.py
@@ -0,0 +1,17 @@
+routes ={
+    "GET":[
+        "http://localhost:5000/api/students",
+        "http://localhost:5000/api/teacher",
+        "http://localhost:5000/api/courses",
+        "http://localhost:5000/api/attendence",
+    ],
+    "POST":[
+
+    ],
+    "PUT":[
+
+    ],
+    "DELETE":[
+
+    ]
+}
--- backend/controllers/StudentController.js
+++ backend/controllers/StudentController.js
@@ -3,7 +3,6 @@ const Student = require('../models/StudentModel');
 
 async function getStudents(request,res){
     try {
-        // const students = await Student.getStudentData();
         const students = await Student.fetchAllStudent();
         res.writeHeader(200,{"Content-Type":'application/json'});
         res.end(JSON.stringify(students));
