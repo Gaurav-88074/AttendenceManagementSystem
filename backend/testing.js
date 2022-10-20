e1610707ba95c821a1c6093cb1d64164f97353a0
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-20 11:15:29 +0530

    made some changes

--- backend/testing.js
+++ backend/testing.js
@@ -1,10 +1,11 @@
 const { v4: uuidv4 } = require('uuid');
-const Pool = require('pg').Pool
-const pool = new Pool({
-  user: 'postgres',
-  host: 'containers-us-west-95.railway.app',
-  database: 'railway',
-  password: '8PwVk9SRnYmvkbAesJUv',
-  port: 5821,
-})
-pool.query("select * from teacher").then((res)=>console.log(res['rows']));
+console.log(uuidv4());
+// const Pool = require('pg').Pool
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'containers-us-west-95.railway.app',
+//   database: 'railway',
+//   password: '8PwVk9SRnYmvkbAesJUv',
+//   port: 5821,
+// })
+// pool.query("select * from teacher").then((res)=>console.log(res['rows']));
