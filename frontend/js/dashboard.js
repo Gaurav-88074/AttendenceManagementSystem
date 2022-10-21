641bd5da08f26e7327318989fe84c378264e84eb
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2022-10-21 13:22:24 +0530

    dummy added

--- frontend/js/dashboard.js
+++ frontend/js/dashboard.js
@@ -4,4 +4,10 @@ window.onload = function() {
         console.log("lol");
         window.location.href = "../components/create.html";
     })
+    document.querySelectorAll('.course-card').forEach((node)=>{
+        node.addEventListener("click",(event)=>{
+            console.log("lol");
+            window.location.href = "./students.html";
+        })
+    })
 }
