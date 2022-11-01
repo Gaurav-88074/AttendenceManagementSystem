c4b994890b9f76732a08f31e3fa7b6bdf7d5b902
Gaurav-88074 <81903385+Gaurav-88074@users.noreply.github.com>
2022-11-01 14:09:36 +0530

    route fixed

--- frontend/js/signup.js
+++ frontend/js/signup.js
@@ -1,15 +1,15 @@
-window.onload = function(params) {
-    document.querySelector(".logo-name").addEventListener("click",(e)=>{
+window.onload = function (params) {
+    document.querySelector(".logo-name").addEventListener("click", (e) => {
         window.location.href = "./dashboard.html";
     })
-    document.getElementById("login-button").addEventListener("click",(e)=>{
+    document.getElementById("login-button").addEventListener("click", (e) => {
         window.location.href = "./login.html";
     })
-    document.getElementById("signupButton").addEventListener("click",()=>{
+    document.getElementById("signupButton").addEventListener("click", () => {
         const email = document.getElementById("emailInput").value;
         const username = document.getElementById("nameInput").value;
         const password = document.getElementById("passwordInput").value;
-        if(email.length==0 || username.length==0 ||password.length==0){
+        if (email.length == 0 || username.length == 0 || password.length == 0) {
             alert("input fields cannot be empty ");
             return;
         }
@@ -22,17 +22,20 @@ window.onload = function(params) {
                 "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                teacherName : username,
-                email_id : email,
-                password : password
+                teacherName: username,
+                email_id: email,
+                password: password
             }),
         };
-        
+
         // fetch("http://localhost:5000/api/add/teacher", options)
         fetch("https://attendencemanagementsystem-production.up.railway.app/add/teacher", options)
             .then((response) => response.json())
-            .then((response) => console.log(response))
+            .then((response) => { 
+                console.log(response);
+                window.location.href = "./login.html"; 
+            })
             .catch((err) => console.error(err));
-        
+
     })
 }
